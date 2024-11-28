f58742136a757d7f05a64f366171b357050cefec
kinghzrd <86816435+kinghzrd@users.noreply.github.com>
2024-11-28 17:15:23 +1300

    Prototyping a radial progress bar Works, but I'll probably replace the wonky outline implementation and just apply a texture to the generated polygon background instead.

--- common/src/main/java/vice/sol_valheim/FoodHUD.java
+++ common/src/main/java/vice/sol_valheim/FoodHUD.java
@@ -1,15 +1,13 @@
 package vice.sol_valheim;
 
 
-
-import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
 import dev.architectury.event.events.client.ClientGuiEvent;
 import dev.architectury.platform.Platform;
 import net.minecraft.client.Minecraft;
 
 #if PRE_CURRENT_MC_1_19_2
-
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.world.level.Level;
 import com.mojang.blaze3d.platform.GlStateManager;
@@ -20,6 +18,7 @@ import net.minecraft.client.gui.GuiComponent;
 #elif POST_CURRENT_MC_1_20_1
 
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.util.CommonColors;
 
 #endif
@@ -29,10 +28,13 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FastColor;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.UseAnim;
+import org.joml.Matrix4f;
+import org.joml.Vector2d;
 import vice.sol_valheim.accessors.PlayerEntityMixinDataAccessor;
 
 public class FoodHUD implements ClientGuiEvent.RenderHud
@@ -82,15 +84,17 @@ public class FoodHUD implements ClientGuiEvent.RenderHud
             return;
 
         var isDrink = food.item.getDefaultInstance().getUseAnimation() == UseAnim.DRINK;
-        int bgColor = isDrink ? FastColor.ARGB32.color(96, 52, 104, 163) : FastColor.ARGB32.color(96, 0, 0, 0);
-        int yellow = FastColor.ARGB32.color(255, 255, 191, 0);
+        int white = FastColor.ARGB32.color(255, 255, 255, 255);
+        int whiteBg = FastColor.ARGB32.color(128, 255, 255, 255);
+        int yellow = FastColor.ARGB32.color(255, 255, 200, 37);
+        int yellowBg = FastColor.ARGB32.color(150, 255, 200, 37);
 
         int startWidth = width - (size * offset) - offset + 1;
         float ticksLeftPercent = Float.min(1.0F, (float) food.ticksLeft / foodConfig.getTime());
-        int barHeight = Integer.max(1, (int)((size + 2f) * ticksLeftPercent));
-        int barColor = ticksLeftPercent < SOLValheim.Config.common.eatAgainPercentage ?
-                FastColor.ARGB32.color(180, 255, 10, 10) :
-                FastColor.ARGB32.color(96, 0, 0, 0);
+        boolean canEat = ticksLeftPercent < SOLValheim.Config.common.eatAgainPercentage;
+        int bgColor = isDrink ? FastColor.ARGB32.color(96, 52, 104, 163) : FastColor.ARGB32.color(96, 0, 0, 0);
+        int barColor = canEat ? yellow : white;
+        int barBgColor = canEat ? yellowBg : whiteBg;
 
         var time = (float) food.ticksLeft / (20 * 60);
         var scale = useLargeIcons ? 0.75f : 0.5f;
@@ -103,10 +107,10 @@ public class FoodHUD implements ClientGuiEvent.RenderHud
             time =  (float) food.ticksLeft / 20;
         }
 
-        var pose = #if PRE_CURRENT_MC_1_19_2 graphics #elif POST_CURRENT_MC_1_20_1 graphics.pose(); #endif;
+        var pose = #if PRE_CURRENT_MC_1_19_2 graphics #elif POST_CURRENT_MC_1_20_1 graphics.pose() #endif;
 
         fill(graphics, startWidth, height, startWidth + size, height + size, bgColor);
-        fill(graphics, startWidth, Integer.max(height, height - barHeight + size), startWidth + size, height + size, barColor);
+        fillCircularBar(graphics, size, size, startWidth, height, barColor, barBgColor, ticksLeftPercent);
 
         pose.pushPose();
         pose.scale(scale, scale, scale);
@@ -139,6 +143,109 @@ public class FoodHUD implements ClientGuiEvent.RenderHud
         GuiComponent.fill(graphics, width, height, x, y, color);
         #elif POST_CURRENT_MC_1_20_1
         graphics.fill(width, height, x, y, color);
+        #endif
+    }
+
+    // There's probably a function to make this easier but idk
+    private static Vector2d calcCircularCoords(float alpha) {
+        var angle = alpha * 2 * Math.PI;
+        var clampedAngle = angle % (Math.PI / 2);
+        var a = Math.min(Math.cos(clampedAngle)/Math.sin(clampedAngle), 1);
+        var b = Math.min(Math.tan(clampedAngle), 1);
+
+        double a2,b2;
+        if (angle < Math.PI / 2) {
+            a2 = -b;
+            b2 = -a;
+        } else if (angle < Math.PI) {
+            a2 = -a;
+            b2 = b;
+        } else if (angle < Math.PI / 2 * 3) {
+            a2 = b;
+            b2 = a;
+        } else {
+            a2 = a;
+            b2 = -b;
+        }
+
+        return new Vector2d(a2, b2);
+    }
+
+    private static void fillCircularBar(GuiGraphics graphics, int width, int height, int x, int y, int color, int bgColor, float alpha) {
+        #if PRE_CURRENT_MC_1_19_2
+        // todo
+        #elif POST_CURRENT_MC_1_20_1
+        Matrix4f matrix4f = graphics.pose().last().pose();
+        Tesselator tesselator = Tesselator.getInstance();
+        BufferBuilder buffer = tesselator.getBuilder();
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+        RenderSystem.enableBlend();
+
+        int middleX = x + (width / 2);
+        int middleY = y + (height / 2);
+
+        // BACKGROUND
+        buffer.begin(VertexFormat.Mode.TRIANGLE_FAN, DefaultVertexFormat.POSITION_COLOR);
+
+        // Center Vertex
+        if (alpha < 1.00) {
+            buffer.vertex(matrix4f, middleX, middleY, 0).color(bgColor).endVertex();
+        }
+        // Start Vertex - TOP CENTRE
+        buffer.vertex(matrix4f, middleX, y, 0).color(bgColor).endVertex();
+        // Intermediate Vertices
+        if (alpha > 0.125) // TOP LEFT
+        {
+            buffer.vertex(matrix4f, x, y, 0).color(bgColor).endVertex();
+        }
+        if (alpha > 0.375) // BOTTOM LEFT
+        {
+            buffer.vertex(matrix4f, x, y + height, 0).color(bgColor).endVertex();
+        }
+        if (alpha > 0.625)  // BOTTOM RIGHT
+        {
+            buffer.vertex(matrix4f, x + width, y + height, 0).color(bgColor).endVertex();
+        }
+        if (alpha > 0.875) // TOP RIGHT
+        {
+            buffer.vertex(matrix4f, x + width, y, 0).color(bgColor).endVertex();
+        }
+        // Endpoint Vertex
+        if (alpha < 1.00) {
+            Vector2d ePos = calcCircularCoords(alpha);
+            buffer.vertex(matrix4f, (float) (middleX + (ePos.x * width / 2)), (float) (middleY + (ePos.y * height / 2)), 0).color(bgColor).endVertex();
+        }
+        tesselator.end();
+
+        // OUTLINE
+        // Couldn't figure out how to get a buffer builder to render a line, using GuiGraphics instead...
+        // There some weirdness with this current implementation, probably due to rounding.
+        // Start line - TOP LEFT
+        var ta = Math.min(alpha, 0.125) / 0.125;
+        graphics.hLine((int) Mth.lerp(ta, middleX - 1, x), middleX - 1, y, color);
+        // Intermediate lines
+        if (alpha > 0.125) // LEFT
+        {
+            ta = (Math.min(alpha, 0.375) - 0.125) / 0.25 ;
+            graphics.vLine(x, (int) Mth.lerp(ta, y, y + height - 1), y, color);
+        }
+        if (alpha > 0.375) // BOTTOM
+        {
+            ta = (Math.min(alpha, 0.625) - 0.375) / 0.25 ;
+            graphics.hLine(x, (int) Mth.lerp(ta, x, x + width - 1), y + height - 1, color);
+        }
+        if (alpha > 0.625)  // RIGHT
+        {
+            ta = (Math.min(alpha, 0.875) - 0.625) / 0.25 ;
+            graphics.vLine(x + width - 1, (int) Mth.lerp(ta, y + height - 1, y), y + height - 1, color);
+        }
+        if (alpha > 0.875) // TOP RIGHT
+        {
+            ta = (alpha - 0.875) / 0.125 ;
+            graphics.hLine((int) Mth.lerp(ta, x + width - 1, middleX), x + width - 1, y, color);
+        }
+
+        RenderSystem.disableBlend();
         #endif
     }
 
